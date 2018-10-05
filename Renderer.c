849958b8d1e245364b83b09ac420819bd2eb0c2c
Kasania <nasang1997@naver.com>
2018-10-06 00:12:24 +0900

    Add : Rendering Object Struct and Function

--- Renderer.c
+++ Renderer.c
@@ -12,10 +12,35 @@
 */
 
 #include <Windows.h>
-#include <stdio.h>
+typedef struct sImage {
+	BITMAP bitmap;
+	HDC Image;
+	HDC Mask;
+	int width;
+	int height;
+}sImage;
+
+typedef struct RenderingObject {
+	sImage img;
+	int x;
+	int y;
+}RenderingObject;
+
+typedef struct RenderingQueueObject {
+	RenderingObject obj;
+	void(*Render)(RenderingObject obj);
+	struct RenderingQueueObject *prev;
+	struct RenderingQueueObject *next;
+}RenderingQueueObject;
+
+typedef struct RenderingQueue {
+	long size;
+	RenderingQueueObject * HEAD;
+}RenderingQueue;
 
 void initialzeScreen(int XSize, int YSize);
 void adjustScreenSize();
+void RenderObject(RenderingObject obj);
 
 HANDLE Console;
 HWND Window;
@@ -23,7 +48,6 @@ HDC FrontDC;
 HDC BackDC;
 HBITMAP RenderBuffer;
 int ScreenXSize, ScreenYSize;
-
 
 void initialzeScreen(int XSize, int YSize) {
 	ScreenXSize = XSize;
@@ -39,13 +63,28 @@ void initialzeScreen(int XSize, int YSize) {
 	Cursor.dwSize = 1;
 	system("echo off");
 	system("cls");
+	SMALL_RECT size = { 0,0,77,47 };
+	SetConsoleWindowInfo(Console, TRUE, &size);
 
-	//adjustScreenSize();
-	
+	//adjustScreenSize();	
+	//RenderingQueueObject rqo;
+	//rqo.Render = RenderObject;
+	//rqo.Render(rqo.obj);
+
 }
 
 void adjustScreenSize() {
-	HKEY key;
+	//HKEY key;
 	//Registry 작업, 사지방에서 불가능 함
 	//나중으로 미룸
+}
+
+void RenderObject(RenderingObject obj) {
+	if (obj.img.Mask != NULL) {
+		BitBlt(BackDC, obj.x, obj.y, obj.img.width, obj.img.height, obj.img.Mask, 0, 0, SRCPAINT);
+		BitBlt(BackDC, obj.x, obj.y, obj.img.width, obj.img.height, obj.img.Image, 0, 0, SRCAND);
+	}
+	else {
+		BitBlt(BackDC, obj.x, obj.y, obj.img.width, obj.img.height, obj.img.Image, 0, 0, SRCCOPY);
+	}
 }
